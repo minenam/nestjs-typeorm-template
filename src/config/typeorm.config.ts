35b16c7d0b3c87a805b4ef75aa88553907716fb3
minenam <nhm6947@gmail.com>
2022-10-19 00:44:54 +0900

    feat: TypeORM, DB 설정

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+export const typeOrmAsyncModuleOptions = {
+  useFactory: async (): Promise<TypeOrmModuleOptions> => ({
+    namingStrategy: new SnakeNamingStrategy(),
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: process.env.MODE == 'dev', //! set 'false' in production
+    autoLoadEntities: true,
+    logging: process.env.MODE == 'dev',
+  }),
+};
